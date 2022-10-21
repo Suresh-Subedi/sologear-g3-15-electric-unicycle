2ef8f7c0379130962742d19f755ecc3c0700a116
Suresh-Subedi <7327694+Suresh-Subedi@users.noreply.github.com>
2022-10-21 19:23:38 +0200

    add kicad files

--- g3-15-custom-firmware/src/main.cpp
+++ g3-15-custom-firmware/src/main.cpp
@@ -1,22 +1,30 @@
 #include <Arduino.h>
 
-int HIn1 = PB10;
-int HIn2 = PB9;
-int HIn3 = PB8;
+int HIn1 = PA10;
+int HIn2 = PA9;
+int HIn3 = PA8;
 int LIn1 = PB15;
 int LIn2 = PB14;
 int LIn3 = PB13;
 
+// Hall sensors 3.3V
+int HS1 = PA0; // Blue Pin 2
+int HS2 = PA1; // Yellow Pin 3
+int HS3 = PA2; // Green Pin 4
+
 int pins_h[] = {HIn1, HIn2, HIn3};
 int pins_l[] = {LIn1, LIn2, LIn3};
 
-int delay_ms = 200;
+int delay_ms = 2000;
 
 int POS = 1;
 int OFF = 0;
 int NEG = -1;
 
 bool invert = true;
+bool turnOff = true;
+bool delayAfterOff = false;
+int magnetise_ms = 20;
 
 void setState(int state, int pin);
 
@@ -39,6 +47,10 @@ void setup()
     pinMode(LIn1, OUTPUT);
     pinMode(LIn2, OUTPUT);
     pinMode(LIn3, OUTPUT);
+
+    pinMode(HS1, INPUT);
+    pinMode(HS2, INPUT);
+    pinMode(HS3, INPUT);
 }
 
 void loop()
@@ -56,54 +68,60 @@ void s1()
     u(POS);
     v(NEG);
     w(OFF);
-    delay(delay_ms);
+    if(!delayAfterOff) { delay(magnetise_ms); }
     u(OFF);
     v(OFF);
+    delay(delay_ms);
 }
 void s2()
 {
     u(POS);
     v(OFF);
     w(NEG);
-    delay(delay_ms);
+    if(!delayAfterOff) { delay(magnetise_ms); }
     u(OFF);
     w(OFF);
+    delay(delay_ms);
 }
 void s3()
 {
     u(OFF);
     v(POS);
     w(NEG);    
-    delay(delay_ms);
+    if(!delayAfterOff) { delay(magnetise_ms); }
     v(OFF);
     w(OFF);
+    delay(delay_ms);
 }
 void s4()
 {
     u(NEG);
     v(POS);
     w(OFF);
-    delay(delay_ms);
+    if(!delayAfterOff) { delay(magnetise_ms); }
     u(OFF);
     v(OFF);
+    delay(delay_ms);
 }
 void s5()
 {
     u(NEG);
     v(OFF);
     w(POS);
-    delay(delay_ms);
+    if(!delayAfterOff) { delay(magnetise_ms); }
     u(OFF);
     w(OFF);
+    delay(delay_ms);
 }
 void s6()
 {
     u(OFF);
     v(NEG);
     w(POS);
-    delay(delay_ms);
+    if(!delayAfterOff) { delay(magnetise_ms); }
     v(OFF);
     w(OFF);
+    delay(delay_ms);
 }
 
 void u(int state)
