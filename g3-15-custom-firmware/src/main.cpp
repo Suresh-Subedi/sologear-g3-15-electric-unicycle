50fef77881664e5042e04581b1b80c256bd5b965
Suresh-Subedi <7327694+Suresh-Subedi@users.noreply.github.com>
2023-02-05 22:34:57 +0100

    wip

--- g3-15-custom-firmware/src/main.cpp
+++ g3-15-custom-firmware/src/main.cpp
@@ -1,194 +1,48 @@
 #include <Arduino.h>
 
-// W
-int HIn1 = PA10;
-int LIn1 = PB15;
-// V
-int HIn2 = PA9;
-int LIn2 = PB14;
-// U
-int HIn3 = PA8;
-int LIn3 = PB13;
-int pins_led[] = {PA15, PB4, PB5, PB8};
-int pins_h[] = {HIn1, HIn2, HIn3};
-int pins_l[] = {LIn1, LIn2, LIn3};
+// Hall-effect sensor 3.3V
+int HS1 = PA0; // Blue Pin 2
+int HS2 = PA1; // Yellow Pin 3
+int HS3 = PA2; // Green Pin 4
 
-int buzzer = PB3;
+int led1 = PA15;
+int led2 = PB4;
+int led3 = PB5;
+int led4 = PB8;
+int leds[] = { led1, led2, led3, led4 };
+// int leds[] = { led4, led3, led2, led1 };
+int powerLed = PB9;
 
-bool POS = HIGH;
-bool NEG = LOW;
-bool OFF = -1;
+void setup () {  
+  pinMode(HS1, INPUT);
+  pinMode(HS2, INPUT);
+  pinMode(HS3, INPUT);
 
-void s1();
-void s2();
-void s3();
-void s4();
-void s5();
-void s6();
-
-void w(int state);
-void v(int state);
-void u(int state);
-
-void setState(int pin, int state);
-
-int exeStep(int step);
-
-void setup()
-{
-
-  // Mosfet driver pins
-  pinMode(HIn1, OUTPUT);
-  pinMode(HIn2, OUTPUT);
-  pinMode(HIn3, OUTPUT);
-  pinMode(LIn1, OUTPUT);
-  pinMode(LIn2, OUTPUT);
-  pinMode(LIn3, OUTPUT);
-
-  // Buzzer
-  pinMode(buzzer, OUTPUT);
+  pinMode(powerLed, OUTPUT);
 
   // LEDs
-  pinMode(PA15, OUTPUT); //1
-  pinMode(PB4, OUTPUT);
-  pinMode(PB5, OUTPUT);
-  pinMode(PB8, OUTPUT); //4
-
-  // Inverted mosfet driver pins
-  digitalWrite(LIn1, HIGH);
-  digitalWrite(LIn2, HIGH);
-  digitalWrite(LIn3, HIGH);
-
-  // Hall-effect sensor 3.3V
-  int HS1 = PA0; // Blue Pin 2
-  int HS2 = PA1; // Yellow Pin 3
-  int HS3 = PA2; // Green Pin 4
-}
-int step = 1;
-uint32_t interval = 2000;
-void loop()
-{
-  /*static uint32_t nextTime;
-  if (millis() - nextTime >= interval) {
-    nextTime += interval;
-    step = exeStep(step);
-  }*/
-  digitalWrite(PA15, LOW);
-  digitalWrite(PB4, LOW);
-  digitalWrite(PB5, LOW);
-  digitalWrite(PB8, LOW);
-
-  //digitalWrite(buzzer, HIGH);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 }
 
-int exeStep(int step)
-{
-  switch (step)
-  {
-  case 1:
-    s1();
-    break;
-  case 2:
-    s2();
-    break;
-  case 3:
-    s3();
-    break;
-  case 4:
-    s4();
-    break;
-  case 5:
-    s5();
-    break;
-  case 6:
-    s6();
-    break;
-  default:
-    break;
-  }
-  return (step + 1) % 6;
-}
-
-void s1()
-{
-  v(OFF);
-  w(NEG);
-  u(POS);
-}
-void s2()
-{
-  u(OFF);
-  w(NEG);
-  v(POS);
-}
-void s3()
-{
-  w(OFF);
-  u(NEG);
-  v(POS);
-}
-void s4()
-{
-  u(NEG);
-  w(POS);
-  v(OFF);
-}
-void s5()
-{
-  u(OFF);
-  w(NEG);
-  v(POS);
-}
-void s6()
-{
-  v(OFF);
-  w(NEG);
-  u(POS);
-}
-
-void w(int state)
-{
-  setState(1, state);
-}
-void v(int state)
-{
-  setState(2, state);
-}
-void u(int state)
-{
-  setState(3, state);
-}
-
-void setState(int pin, int state)
-{
-  int hpin = pins_led[pin - 1];
-  if (state == POS)
-  {
-    digitalWrite(hpin, HIGH);
-  }
-  else if (state == NEG)
-  {
-    digitalWrite(hpin, LOW);
+void display(byte number) {
+  for(byte i = 0; i < 4; i++) {
+    bool value = number & 1;
+    digitalWrite(leds[i], !value);
+    number /= 2;
   }
 }
 
-/*void setState(int pin, int state)
-{
-  int hpin = pins_h[pin - 1];
-  int lpin = pins_l[pin - 1];
-  if (state == POS)
-  {
-    digitalWrite(lpin, HIGH);
-    digitalWrite(hpin, HIGH);
-  }
-  else if (state == NEG)
-  {
-    digitalWrite(lpin, LOW);
-    digitalWrite(hpin, LOW);
-  }
-  else if (state == OFF)
-  {
-    digitalWrite(lpin, HIGH);
-    digitalWrite(hpin, LOW);
-  }
-}*/
+byte i = 0;
+
+void loop() {
+  digitalWrite(powerLed, LOW);
+
+  int angle = analogRead(HS1);
+  display(i);
+  i += 1;
+  i = i % 16;
+  delay(1000);
+}
